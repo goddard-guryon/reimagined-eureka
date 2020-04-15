c9c27f246d9e8958af91e067f5a05d176e4229b5
Goddard Guryon <aayush.srivastav925@gmail.com>
2020-04-16 01:13:16 +0530

    some progress in the blockchain class, I'm really enjoying copying code so far, but I still feel implementing the Node class is gonna be a lot more difficult, I might even leave the project before starting the Node class :/

--- eureka.py
+++ eureka.py
@@ -16,7 +16,7 @@ from multiprocessing import Lock  # need this for threading in blockchain
 from pymongo import MongoClient  # need this for storing local blockchain
 from mongoengine import *   # I didn't want to use SQL as it's too generic & slow, + I haven't used MongoDB in a while
 import security  # need this for...well...everything related to cryptography
-# from typing import Union, Optional
+from typing import Union, Optional
 
 
 class Transaction(object):
@@ -222,7 +222,7 @@ class BlockChain(object):
 
         return
 
-    def add_block(self, block):
+    def add_block(self, block: Block) -> None:
         status = False
 
         # check if we're adding to the tallest branch
@@ -267,16 +267,153 @@ class BlockChain(object):
                           'branch': current_branch}
             _ = transaction_entry.insert_one(entry_data)
 
-        # we update our branch database by deleting all current entries in it and then adding our latest entry to it
+        # we update the height and hash of current branch by looking for the branch ID
         branch_entry = self.database.branch
-        _ = branch_entry.delete_many({})
-        entry_data = {'id': current_branch,
-                      'current_height': block.height,
-                      'current_hash': block.header}
-        _ = branch_entry.insert_one(entry_data)
+        to_find = {'_id': current_branch}
+        to_update = {'current_height': block.height,
+                     'current_hash': block.header}
+        _ = branch_entry.update_one(to_find, to_update)
+
+    def get_new_branch_id(self, block_hash: str, height: int):
+        # insert a new branch
+        branch_entry = self.database.branch
+        entry_data = {'current_height': height,
+                      'current_hash': block_hash}
+        new_branch = branch_entry.insert_one(entry_data)
+
+        # return the ID of this new branch
+        return new_branch.inserted_id
+
+    def prune(self) -> str:
+        """
+        Removes all transactions, blocks and branches that are not a part of the main branch of blockchain
+        :return: number of transactions, blocks and branches removed (str)
+        """
+        # initialize the collections
+        blocks_entry = self.database.blocks
+        transaction_entry = self.database.transactions
+        branch_entry = self.database.branch
+
+        # find the maximum height in the blockchain
+        max_height = [x for x in blocks_entry.find().sort('height', -1).limit(1)][0]['height']
+
+        # find the IDs of all branches that have this height
+        target_branches = branch_entry.find({'current_height': {
+            '$lt': (max_height - self.SHORT_CHAIN_TOLERANCE)}})['_id']
+
+        # find all transactions, blocks and branches that belong to `target_branches`
+        tran_block_query = {'branch': {'$in': target_branches}}
+        branch_query = {'_id': {'$in': target_branches}}
+
+        # delete all such transactions, blocks and branches
+        block_del = blocks_entry.delete_many(tran_block_query)
+        transac_del = transaction_entry.delete_many(tran_block_query)
+        branch_del = branch_entry.delete_many(branch_query)
+
+        # return the number of transactions, blocks and branches removed
+        return f"Cleared {transac_del.deleted_count} transactions, {block_del.deleted_count} blocks, and " \
+               f"{branch_del.deleted_count} branches after pruning the blockchain"
+
+    def change_primary_branch(self, branch) -> None:
+        # get info of the last block
+        new_header, new_branch, new_height = self.get_tallest_block(branch)
+
+        # initialize variables
+        alt_hashes = []
+        start = new_height
+        stop = new_height
+        end_of_branch = False
+
+        # start searching
+        while not end_of_branch:
+            # move one block back
+            new_header, new_branch, new_height = self.hash_to_header(new_header.prev_hash)
+
+            # if we're still at the alternate branch, just add this block to the pile
+            if new_branch > 0:
+                alt_hashes.append(new_header.hash)
+
+            # if we've reached the split point, end our search
+            else:
+                start = new_height
+                end_of_branch = True
+
+        # now that we've found the correct primary branch, fetch headers of all blocks in this branch
+        primary_branch = [block[0] for block in self.get_range_of_headers(start, stop, branch=0)]
+
+        # get the database
+        block_entry = self.database.blocks
+        transaction_entry = self.database.transactions
+
+        # if this block belongs to [originally] alternate branch, shift this to primary branch
+        _ = block_entry.update_many({'hash': {'$in': alt_hashes}}, {'branch': 0})
+
+        # if this block belongs to the new alternate branch, shift it to the branch number given
+        _ = block_entry.update_many({'hash': {'$in': primary_branch}}, {'branch': branch})
+
+        # same for all the transactions
+        _ = transaction_entry.update_many({'block_hash': {'$in': alt_hashes}}, {'branch': 0})
+        _ = transaction_entry.update_many({'block_hash': {'$in': primary_branch}}, {'branch': branch})
+
+        # done
+        return
+
+    def get_current_branch(self, hash_key: str) -> Union[Optional[int], Block]:
+        """
+        get the block object from its hash value
+        :param hash_key: the hash value to search for in all blocks (str)
+        :return: the corresponding block (Block) or 0 if no such block exists (int)
+        """
+        # get the database
+        block_entry = self.database.blocks
+
+        # find the relevant block using the given hash key
+        result = block_entry.find({'hash': hash_key})
+
+        # if no such block exists, return 0
+        if result.count() == 0:
+            return 0
+
+        # return the corresponding block
+        return result[0]
+
+    def get_height(self) -> int:
+        """
+        get the maximum height in the blocks...it works as follows:
+            call the collection by self.database.blocks
+            get all entries in the collection by .find()
+            sort these entries by height in descending order using .sort('height', -1) (-1 specifies descending)
+            grab the first entry in this using .limit(1)
+            we get a cursor object, which we convert to a list of 1 entry then just one entry by slicing
+            this gives us the entry as a dict, from which we retrieve the height value
+        """
+        return [x for x in self.database.blocks.find().sort('height', -1).limit(1)][0]['height']
+
+    def get_clashing_branches(self, prev_hash: str) -> list:
+        # find all blocks that have the given previous hash and return the branch values in those blocks
+        return [x['branch'] for x in self.database.blocks.find({'previous_hash': prev_hash}).sort('branch')]
+
+    def get_tallest_block(self, branch: int = 0) -> Union[Optional[None], tuple]:
+        # call the collection
+        blocks_entry = self.database.blocks
+
+        # find the maximum height (honestly, at this point, I think I should make this a separate function)
+        max_height = [x for x in blocks_entry.find().sort('branch', -1).limit(1)][0]['height']
+
+        # find all blocks in this branch, then find the tallest blocks from that list
+        # it should be possible to do in a single step, but I'm too lazy to find out how
+        block_list = blocks_entry.find({'branch': branch})
+        final_block = block_list.find({'height': max_height})
+
+        # if we have no block as tall as we want, return nothing
+        if final_block.count() == 0:
+            return None
+
+        # otherwise, return its header, branch and height
+        return final_block[0]['header'], final_block[0]['branch'], final_block[0]['height']
 
 
-# instantiate exceptions here
+# initialize exceptions here
 class BlockChainException(Exception):
     def __init(self, index, message):
         super(BlockChainException, self).__init__(message)
